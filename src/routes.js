c958556ccf2daddcf1dcd8a558257b5ff07826e4
wanderson <wanderson.alves.rodrigues@gmail.com>
2019-05-05 14:13:16 -0300

    Criação controller oficina

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const VeiculoController = require('./controllers/veiculo.controller');
+const OficinaController = require('./controllers/oficina.controller');
 
 const routes = express.Router();
 
 routes.get('/veiculos', VeiculoController.show);
+routes.get('/oficinas', OficinaController.show);
 
 module.exports = routes;
